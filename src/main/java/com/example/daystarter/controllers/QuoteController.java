2c28db17bddffd5a2cc9dd85d68d0f028ed32a39
bigboytimi <olutimiolowookere@icloud.com>
2023-08-07 06:44:49 +0100

    daystarterX

--- src/main/java/com/example/daystarter/controllers/QuoteController.java
+++ src/main/java/com/example/daystarter/controllers/QuoteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/quotes")
+@RequestMapping("api/v1/quote")
 @RequiredArgsConstructor
 public class QuoteController {
 
